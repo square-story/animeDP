35f2449738622d559cd26ffa78c65c18a4ff293c
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-20 12:18:31 +0530

    background is seted

--- src/pages/MasterPage.jsx
+++ src/pages/MasterPage.jsx
@@ -1,9 +1,10 @@
 import Card from "../components/Card"
 import HomeButtons from "../components/HomeButtons"
+import './MasterPage.css'
 
 const MasterPage = () => {
     return (
-        <div className="flex flex-col gap-10 justify-center h-screen w-full items-center bg-black text-white">
+        <div className="pg-com flex flex-col gap-10 justify-center h-screen w-full items-center bg-black text-white">
             <Card />
             <HomeButtons />
         </div>
