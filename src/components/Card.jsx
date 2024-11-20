7d99676fd468b799d6f8caf87fbc0059cc9e9866
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-20 17:06:44 +0530

    loading spinning added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,24 @@
 import { useSelector } from "react-redux";
 
 const Card = () => {
-    const { imageUrl, error } = useSelector((state) => state.api);
+    const { imageUrl, loading, error } = useSelector((state) => state.api);
+
     return (
         <>
             <div
                 className="relative drop-shadow-xl w-48 h-64 overflow-hidden rounded-xl bg-[rgb(0,0,0)] group hover:scale-110 transition-transform duration-300 ease-out"
             >
-                <div className="absolute inset-0 z-[1]">
+                <div className="absolute inset-0 z-[1] flex items-center justify-center">
+                    {loading && (
+                        <div
+                            className="flex flex-col bg-neutral-300 w-56 h-64 animate-pulse rounded-xl p-4 gap-4"
+                        >
+                            <div className="bg-neutral-400/50 w-full h-32 animate-pulse rounded-md"></div>
+                        </div>
+
+                    )}
                     {error && <p className="text-red-500">{error}</p>}
-                    {imageUrl && (
+                    {imageUrl && !loading && (
                         <img
                             className="w-full h-full object-cover"
                             src={imageUrl}
